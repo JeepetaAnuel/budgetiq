f29154185ba2ba53edd13dfe0f618ec34fe62043
Jeepeta <marcjn2004@gmail.com>
2026-05-18 13:59:11 +0200

    Fix page transition bug on navigate back

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import Sidebar from './Sidebar'
 import Header from './Header'
 import Tutorial from '../ui/Tutorial'
@@ -28,17 +28,14 @@ export default function Layout() {
       <div className="flex flex-col flex-1 overflow-hidden">
         <Header />
         <main className="flex-1 overflow-y-auto p-6 lg:p-8">
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={location.pathname}
-              initial={{ opacity: 0, y: 12 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -12 }}
-              transition={{ duration: 0.2 }}
-            >
-              <Outlet />
-            </motion.div>
-          </AnimatePresence>
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0, y: 12 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <Outlet />
+          </motion.div>
         </main>
       </div>
       {showTutorial && <Tutorial onClose={finishTutorial} />}
